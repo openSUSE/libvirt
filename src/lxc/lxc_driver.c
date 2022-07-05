a31e4bafe38e9cd4acf54fda1dfd0e71a149a33d
Jim Fehlig <jfehlig@suse.com>
2022-07-05 13:50:31 -0600

    [openSUSE] Wait for udev events to be handled after removing veth
    As per http://www.redhat.com/archives/libvir-list/2013-July/msg01279.html,
    wait for udev events to be handled after removing a virtual NIC.
    Any udev rule associated to NIC destroy could happen to run with a new
    device with the same name that is being created.
    
    From: <cbosdonnat@suse.com>

--- src/lxc/lxc_driver.c
+++ src/lxc/lxc_driver.c
@@ -3577,6 +3577,7 @@ lxcDomainAttachDeviceNetLive(virLXCDriver *driver,
         case VIR_DOMAIN_NET_TYPE_NETWORK:
         case VIR_DOMAIN_NET_TYPE_ETHERNET:
             ignore_value(virNetDevVethDelete(veth));
+            virWaitForDevices();
             break;
 
         case VIR_DOMAIN_NET_TYPE_DIRECT:
@@ -4047,6 +4048,7 @@ lxcDomainDetachDeviceNetLive(virDomainObj *vm,
             virDomainAuditNet(vm, detach, NULL, "detach", false);
             goto cleanup;
         }
+        virWaitForDevices();
         break;
 
         /* It'd be nice to support this, but with macvlan
