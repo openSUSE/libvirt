openSUSE/libvirt

commit 8b32c80df089a3612a0448c1a92abc2071d6b6a9

network: put dnsmasq parameters in conf-file instead of command line
This patch changes how parameters are passed to dnsmasq.  Instead of
being on the command line, the parameters are put into a file (one
parameter per line) and a commandline --conf-file= specifies the
location of the file.  The file is located in the same directory as
the leases file.

Putting the dnsmasq parameters into a configuration file
allows them to be examined and more easily understood than
examining the command lines displayed by "ps ax".  This is
especially true when a number of networks have been started.

When the use of dnsmasq was originally done, the required command line
was simple, but it has gotten more complicated over time and will
likely become even more complicated in the future.

Note: The test conf files have all been renamed .conf instead of
.argv, and tests/networkxml2xmlargvdata was moved to
tests/networkxml2xmlconfdata.

src/network/bridge_driver.c
@@ -138,6 +138,16 @@ networkDnsmasqLeaseFileNameFunc networkDnsmasqLeaseFileName =
     networkDnsmasqLeaseFileNameDefault;
 
 static char *
+networkDnsmasqConfigFileName(const char *netname)
+{
+    char *conffile;
+
+    ignore_value(virAsprintf(&conffile, DNSMASQ_STATE_DIR "/%s.conf",
+                             netname));
+    return conffile;
+}
+
+static char *
 networkRadvdPidfileBasename(const char *netname)
 {
     /* this is simple but we want to be sure it's consistently done */
@@ -164,6 +174,7 @@ networkRemoveInactive(struct network_driver *driver,
 {
     char *leasefile = NULL;
     char *radvdconfigfile = NULL;
+    char *configfile = NULL;
     char *radvdpidbase = NULL;
     dnsmasqContext *dctx = NULL;
     virNetworkDefPtr def = virNetworkObjGetPersistentDef(net);
@@ -183,9 +194,13 @@ networkRemoveInactive(struct network_driver *driver,
     if (!(radvdpidbase = networkRadvdPidfileBasename(def->name)))
         goto no_memory;
 
+    if (!(configfile = networkDnsmasqConfigFileName(def->name)))
+        goto no_memory;
+
     /* dnsmasq */
     dnsmasqDelete(dctx);
     unlink(leasefile);
+    unlink(configfile);
 
     /* radvd */
     unlink(radvdconfigfile);
@@ -198,6 +213,7 @@ networkRemoveInactive(struct network_driver *driver,
 
 cleanup:
     VIR_FREE(leasefile);
+    VIR_FREE(configfile);
     VIR_FREE(radvdconfigfile);
     VIR_FREE(radvdpidbase);
     dnsmasqContextFree(dctx);
@@ -609,13 +625,14 @@ networkBuildDnsmasqHostsList(dnsmasqContext *dctx,
 }
 
 
-static int
-networkBuildDnsmasqArgv(virNetworkObjPtr network,
+int
+networkDnsmasqConfContents(virNetworkObjPtr network,
                         const char *pidfile,
-                        virCommandPtr cmd,
+                        char **configstr,
                         dnsmasqContext *dctx,
                         dnsmasqCapsPtr caps ATTRIBUTE_UNUSED)
 {
+    virBuffer configbuf = VIR_BUFFER_INITIALIZER;
     int r, ret = -1;
     int nbleases = 0;
     int ii;
@@ -627,46 +644,48 @@ networkBuildDnsmasqArgv(virNetworkObjPtr network,
     virNetworkIpDefPtr tmpipdef, ipdef, ipv4def, ipv6def;
     bool ipv6SLAAC;
 
+    *configstr = NULL;
+
     /*
-     * NB, be careful about syntax for dnsmasq options in long format.
+     * All dnsmasq parameters are put into a configuration file, except the
+     * command line --conf-file=parameter which specifies the location of
+     * configuration file.
      *
-     * If the flag has a mandatory argument, it can be given using
-     * either syntax:
-     *
-     *     --foo bar
-     *     --foo=bar
-     *
-     * If the flag has a optional argument, it *must* be given using
-     * the syntax:
-     *
-     *     --foo=bar
-     *
-     * It is hard to determine whether a flag is optional or not,
-     * without reading the dnsmasq source :-( The manpage is not
-     * very explicit on this.
+     * All dnsmasq conf-file parameters must be specified as "foo=bar"
+     * as oppose to "--foo bar" which was acceptable on the command line.
      */
 
     /*
      * Needed to ensure dnsmasq uses same algorithm for processing
      * multiple namedriver entries in /etc/resolv.conf as GLibC.
      */
-    virCommandAddArgList(cmd, "--strict-order",
-                              "--domain-needed",
-                              NULL);
 
-    if (network->def->domain) {
-        virCommandAddArgPair(cmd, "--domain", network->def->domain);
-        virCommandAddArg(cmd, "--expand-hosts");
-    }
-    /* need to specify local even if no domain specified */
-    virCommandAddArgFormat(cmd, "--local=/%s/",
-                           network->def->domain ? network->def->domain : "");
+    /* create dnsmasq config file appropriate for this network */
+    virBufferAsprintf(&configbuf,
+                              "##WARNING:  THIS IS AN AUTO-GENERATED FILE. "
+                              "CHANGES TO IT ARE LIKELY TO BE\n"
+                              "##OVERWRITTEN AND LOST.  Changes to this "
+                              "configuration should be made using:\n"
+                              "##    virsh net-edit %s\n"
+                              "## of other applications using the libvirt API.\n"
+                              "##\n## dnsmasq conf file created by libvirt\n"
+                              "strict-order\n"
+                              "domain-needed\n",
+                              network->def->name);
 
-    if (pidfile)
-        virCommandAddArgPair(cmd, "--pid-file", pidfile);
+     if (network->def->domain) {
+        virBufferAsprintf(&configbuf,
+                 "domain=%s\n"
+                 "expand-hosts\n",
+                 network->def->domain);
+     }
+     /* need to specify local even if no domain specified */
+    virBufferAsprintf(&configbuf,
+                "local=/%s/\n",
+                network->def->domain ? network->def->domain : "");
 
-    /* *no* conf file */
-    virCommandAddArg(cmd, "--conf-file=");
+     if (pidfile)
+        virBufferAsprintf(&configbuf, "pid-file=%s\n", pidfile);
 
     if (dnsmasqCapsGet(caps, DNSMASQ_CAPS_BIND_DYNAMIC)) {
         /* using --bind-dynamic with only --interface (no
@@ -676,15 +695,14 @@ networkBuildDnsmasqArgv(virNetworkObjPtr network,
          * other than one of the virtual guests connected directly to
          * this network). This was added in response to CVE 2012-3411.
          */
-        virCommandAddArgList(cmd,
-                             "--bind-dynamic",
-                             "--interface", network->def->bridge,
-                             NULL);
+        virBufferAsprintf(&configbuf,
+                             "bind-dynamic\n"
+                             "interface=%s\n",
+                             network->def->bridge);
     } else {
-        virCommandAddArgList(cmd,
-                             "--bind-interfaces",
-                             "--except-interface", "lo",
-                             NULL);
+        virBufferAddLit(&configbuf,
+                             "bind-interfaces\n"
+                             "except-interface=lo\n");
         /*
          * --interface does not actually work with dnsmasq < 2.47,
          * due to DAD for ipv6 addresses on the interface.
@@ -701,7 +719,7 @@ networkBuildDnsmasqArgv(virNetworkObjPtr network,
             if (!ipaddr)
                 goto cleanup;
             /* also part of CVE 2012-3411 - if the host's version of
-             * dnsmasq doesn't have --bind-dynamic, only allow listening on
+             * dnsmasq doesn't have bind-dynamic, only allow listening on
              * private/local IP addresses (see RFC1918/RFC3484/RFC4193)
              */
             if (!virSocketAddrIsPrivate(&tmpipdef->address)) {
@@ -710,7 +728,7 @@ networkBuildDnsmasqArgv(virNetworkObjPtr network,
                 virReportError(VIR_ERR_CONFIG_UNSUPPORTED,
                                _("Publicly routable address %s is prohibited. "
                                  "The version of dnsmasq on this host (%d.%d) doesn't "
-                                 "support the --bind-dynamic option, which is required "
+                                 "support the bind-dynamic option, which is required "
                                  "for safe operation on a publicly routable subnet "
                                  "(see CVE-2012-3411). You must either upgrade dnsmasq, "
                                  "or use a private/local subnet range for this network "
@@ -718,27 +736,27 @@ networkBuildDnsmasqArgv(virNetworkObjPtr network,
                                (int)version / 1000000, (int)(version % 1000000) / 1000);
                 goto cleanup;
             }
-            virCommandAddArgList(cmd, "--listen-address", ipaddr, NULL);
+            virBufferAsprintf(&configbuf, "listen-address=%s\n", ipaddr);
             VIR_FREE(ipaddr);
         }
     }
 
     /* If this is an isolated network, set the default route option
      * (3) to be empty to avoid setting a default route that's
-     * guaranteed to not work, and set --no-resolv so that no dns
+     * guaranteed to not work, and set no-resolv so that no dns
      * requests are forwarded on to the dns server listed in the
      * host's /etc/resolv.conf (since this could be used as a channel
      * to build a connection to the outside).
      */
     if (network->def->forward.type == VIR_NETWORK_FORWARD_NONE) {
-        virCommandAddArgList(cmd, "--dhcp-option=3",
-                             "--no-resolv", NULL);
+        virBufferAddLit(&configbuf, "dhcp-option=3\n"
+                                      "no-resolv\n");
     }
 
     for (ii = 0; ii < dns->ntxts; ii++) {
-        virCommandAddArgFormat(cmd, "--txt-record=%s,%s",
-                               dns->txts[ii].name,
-                               dns->txts[ii].value);
+        virBufferAsprintf(&configbuf, "txt-record=%s,%s\n",
+                          dns->txts[ii].name,
+                          dns->txts[ii].value);
     }
 
     for (ii = 0; ii < dns->nsrvs; ii++) {
@@ -774,7 +792,7 @@ networkBuildDnsmasqArgv(virNetworkObjPtr network,
                 goto cleanup;
             }
 
-            virCommandAddArgPair(cmd, "--srv-host", record);
+            virBufferAsprintf(&configbuf, "srv-host=%s\n", record);
             VIR_FREE(record);
             VIR_FREE(recordPort);
             VIR_FREE(recordWeight);
@@ -845,8 +863,8 @@ networkBuildDnsmasqArgv(virNetworkObjPtr network,
                 VIR_FREE(saddr);
                 goto cleanup;
             }
-            virCommandAddArg(cmd, "--dhcp-range");
-            virCommandAddArgFormat(cmd, "%s,%s", saddr, eaddr);
+            virBufferAsprintf(&configbuf, "dhcp-range=%s,%s\n",
+                                        saddr, eaddr);
             VIR_FREE(saddr);
             VIR_FREE(eaddr);
             nbleases += virSocketAddrGetRange(&ipdef->ranges[r].start,
@@ -862,8 +880,7 @@ networkBuildDnsmasqArgv(virNetworkObjPtr network,
             char *bridgeaddr = virSocketAddrFormat(&ipdef->address);
             if (!bridgeaddr)
                 goto cleanup;
-            virCommandAddArg(cmd, "--dhcp-range");
-            virCommandAddArgFormat(cmd, "%s,static", bridgeaddr);
+            virBufferAsprintf(&configbuf, "dhcp-range=%s,static\n", bridgeaddr);
             VIR_FREE(bridgeaddr);
         }
 
@@ -873,16 +890,14 @@ networkBuildDnsmasqArgv(virNetworkObjPtr network,
         /* Note: the following is IPv4 only */
         if (VIR_SOCKET_ADDR_IS_FAMILY(&ipdef->address, AF_INET)) {
             if (ipdef->nranges || ipdef->nhosts)
-                virCommandAddArg(cmd, "--dhcp-no-override");
+                virBufferAddLit(&configbuf, "dhcp-no-override\n");
 
             if (ipdef->tftproot) {
-                virCommandAddArgList(cmd, "--enable-tftp",
-                                     "--tftp-root", ipdef->tftproot,
-                                     NULL);
+                virBufferAddLit(&configbuf, "enable-tftp\n");
+                virBufferAsprintf(&configbuf, "tftp-root=%s\n", ipdef->tftproot);
             }
 
             if (ipdef->bootfile) {
-                virCommandAddArg(cmd, "--dhcp-boot");
                 if (VIR_SOCKET_ADDR_VALID(&ipdef->bootserver)) {
                     char *bootserver = virSocketAddrFormat(&ipdef->bootserver);
 
@@ -890,11 +905,11 @@ networkBuildDnsmasqArgv(virNetworkObjPtr network,
                         virReportOOMError();
                         goto cleanup;
                     }
-                    virCommandAddArgFormat(cmd, "%s%s%s",
+                    virBufferAsprintf(&configbuf, "dhcp-boot=%s%s%s\n",
                                        ipdef->bootfile, ",,", bootserver);
                     VIR_FREE(bootserver);
                 } else {
-                    virCommandAddArg(cmd, ipdef->bootfile);
+                    virBufferAsprintf(&configbuf, "dhcp-boot=%s\n", ipdef->bootfile);
                 }
             }
         }
@@ -907,9 +922,9 @@ networkBuildDnsmasqArgv(virNetworkObjPtr network,
             virReportOOMError();
             goto cleanup;
         }
-        virCommandAddArgFormat(cmd, "--dhcp-leasefile=%s", leasefile);
+        virBufferAsprintf(&configbuf, "dhcp-leasefile=%s\n", leasefile);
         VIR_FREE(leasefile);
-        virCommandAddArgFormat(cmd, "--dhcp-lease-max=%d", nbleases);
+        virBufferAsprintf(&configbuf, "dhcp-lease-max=%d\n", nbleases);
     }
 
     /* this is done once per interface */
@@ -921,19 +936,19 @@ networkBuildDnsmasqArgv(virNetworkObjPtr network,
      * file to allow for runtime additions.
      */
     if (ipv4def || ipv6def)
-        virCommandAddArgPair(cmd, "--dhcp-hostsfile",
-                             dctx->hostsfile->path);
+            virBufferAsprintf(&configbuf, "dhcp-hostsfile=%s\n",
+                                 dctx->hostsfile->path);
 
-    /* Likewise, always create this file and put it on the commandline,
-     * to allow for runtime additions.
+    /* Likewise, always create this file and put it on the commandline, to allow for
+     * for runtime additions.
      */
-    virCommandAddArgPair(cmd, "--addn-hosts",
-                         dctx->addnhostsfile->path);
+    virBufferAsprintf(&configbuf, "addn-hosts=%s\n",
+                       dctx->addnhostsfile->path);
 
     /* Are we doing RA instead of radvd? */
     if (DNSMASQ_RA_SUPPORT(caps)) {
         if (ipv6def)
-            virCommandAddArg(cmd, "--enable-ra");
+            virBufferAddLit(&configbuf, "enable-ra\n");
         else {
             for (ii = 0;
                 (ipdef = virNetworkDefGetIpByIndex(network->def, AF_INET6, ii));
@@ -942,17 +957,21 @@ networkBuildDnsmasqArgv(virNetworkObjPtr network,
                     char *bridgeaddr = virSocketAddrFormat(&ipdef->address);
                     if (!bridgeaddr)
                         goto cleanup;
-                    virCommandAddArgFormat(cmd, "--dhcp-range=%s,ra-only",
-                                           bridgeaddr);
+                    virBufferAsprintf(&configbuf,
+                                      "dhcp-range=%s,ra-only\n", bridgeaddr);
                     VIR_FREE(bridgeaddr);
                 }
             }
         }
     }
 
+    if (!(*configstr = virBufferContentAndReset(&configbuf)))
+        goto cleanup;
+
     ret = 0;
 
 cleanup:
+    virBufferFreeAndReset(&configbuf);
     VIR_FREE(record);
     VIR_FREE(recordPort);
     VIR_FREE(recordWeight);
@@ -960,20 +979,40 @@ cleanup:
     return ret;
 }
 
-int
+/* build the dnsmasq command line */
+static int
 networkBuildDhcpDaemonCommandLine(virNetworkObjPtr network, virCommandPtr *cmdout,
                                   char *pidfile, dnsmasqContext *dctx,
                                   dnsmasqCapsPtr caps)
 {
     virCommandPtr cmd = NULL;
     int ret = -1;
+    char *configfile = NULL;
+    char *configstr = NULL;
 
     network->dnsmasqPid = -1;
 
-    cmd = virCommandNew(dnsmasqCapsGetBinaryPath(caps));
-    if (networkBuildDnsmasqArgv(network, pidfile, cmd, dctx, caps) < 0) {
+    if (networkDnsmasqConfContents(network, pidfile, &configstr, dctx, caps) < 0)
+        goto cleanup;
+    if (!configstr)
+        goto cleanup;
+
+    /* construct the filename */
+    if (!(configfile = networkDnsmasqConfigFileName(network->def->name))) {
+        virReportOOMError();
         goto cleanup;
     }
+
+    /* Write the file */
+    if (virFileWriteStr(configfile, configstr, 0600) < 0) {
+        virReportSystemError(errno,
+                         _("couldn't write dnsmasq config file '%s'"),
+                         configfile);
+        goto cleanup;
+    }
+
+    cmd = virCommandNew(dnsmasqCapsGetBinaryPath(caps));
+    virCommandAddArgFormat(cmd, "--conf-file=%s", configfile);
 
     if (cmdout)
         *cmdout = cmd;
@@ -1298,7 +1337,7 @@ networkStartRadvd(struct network_driver *driver ATTRIBUTE_UNUSED,
     network->radvdPid = -1;
 
     /* Is dnsmasq handling RA? */
-    if (DNSMASQ_RA_SUPPORT(driver->dnsmasqCaps)) {
+   if (DNSMASQ_RA_SUPPORT(driver->dnsmasqCaps)) {
         ret = 0;
         goto cleanup;
     }

src/network/bridge_driver.h
@@ -46,18 +46,19 @@ int networkReleaseActualDevice(virDomainNetDefPtr iface)
 int networkGetNetworkAddress(const char *netname, char **netaddr)
     ATTRIBUTE_NONNULL(1) ATTRIBUTE_NONNULL(2);
 
-int networkBuildDhcpDaemonCommandLine(virNetworkObjPtr network,
-                                      virCommandPtr *cmdout, char *pidfile,
-                                      dnsmasqContext *dctx,
-                                      dnsmasqCapsPtr caps)
-    ;
+int networkDnsmasqConfContents(virNetworkObjPtr network,
+                        const char *pidfile,
+                        char **configstr,
+                        dnsmasqContext *dctx,
+                        dnsmasqCapsPtr caps);
 # else
 /* Define no-op replacements that don't drag in any link dependencies.  */
 #  define networkAllocateActualDevice(iface) 0
 #  define networkNotifyActualDevice(iface) (iface=iface, 0)
 #  define networkReleaseActualDevice(iface) (iface=iface, 0)
 #  define networkGetNetworkAddress(netname, netaddr) (-2)
-#  define networkBuildDhcpDaemonCommandLine(network, cmdout, pidfile, dctx, caps) 0
+#  define networkDnsmasqConfContents(network, pidfile, configstr, \
+                    dctx, caps) 0
 # endif
 
 typedef char *(*networkDnsmasqLeaseFileNameFunc)(const char *netname);

tests/Makefile.am
@@ -56,7 +56,7 @@ EXTRA_DIST =		\
 	networkschematest \
 	networkxml2xmlin \
 	networkxml2xmlout \
-	networkxml2argvdata \
+	networkxml2confdata \
 	nodedevschemadata \
 	nodedevschematest \
 	nodeinfodata     \
@@ -147,7 +147,7 @@ endif
 test_programs += networkxml2xmltest
 
 if WITH_NETWORK
-test_programs += networkxml2argvtest
+test_programs += networkxml2conftest
 endif
 
 if WITH_STORAGE_SHEEPDOG
@@ -431,12 +431,12 @@ networkxml2xmltest_SOURCES = \
 networkxml2xmltest_LDADD = $(LDADDS)
 
 if WITH_NETWORK
-networkxml2argvtest_SOURCES = \
-	networkxml2argvtest.c \
+networkxml2conftest_SOURCES = \
+	networkxml2conftest.c \
 	testutils.c testutils.h
-networkxml2argvtest_LDADD = ../src/libvirt_driver_network_impl.la $(LDADDS)
+networkxml2conftest_LDADD = ../src/libvirt_driver_network_impl.la $(LDADDS)
 else
-EXTRA_DIST += networkxml2argvtest.c
+EXTRA_DIST += networkxml2conftest.c
 endif
 
 if WITH_STORAGE_SHEEPDOG

tests/networkxml2argvdata/dhcp6-nat-network.argv
@@ -1,15 +1,0 @@
-@DNSMASQ@ \
---strict-order \
---domain-needed \
---local=// \
---conf-file= \
---bind-dynamic \
---interface virbr0 \
---dhcp-range 192.168.122.2,192.168.122.254 \
---dhcp-no-override \
---dhcp-range 2001:db8:ac10:fd01::1:10,2001:db8:ac10:fd01::1:ff \
---dhcp-leasefile=/var/lib/libvirt/dnsmasq/default.leases \
---dhcp-lease-max=493 \
---dhcp-hostsfile=/var/lib/libvirt/dnsmasq/default.hostsfile \
---addn-hosts=/var/lib/libvirt/dnsmasq/default.addnhosts \
---enable-ra\

tests/networkxml2argvdata/dhcp6-network.argv
@@ -1,15 +1,0 @@
-@DNSMASQ@ \
---strict-order \
---domain-needed \
---domain=mynet \
---expand-hosts \
---local=/mynet/ \
---conf-file= \
---bind-dynamic \
---interface virbr0 \
---dhcp-range 2001:db8:ac10:fd01::1:10,2001:db8:ac10:fd01::1:ff \
---dhcp-leasefile=/var/lib/libvirt/dnsmasq/default.leases \
---dhcp-lease-max=240 \
---dhcp-hostsfile=/var/lib/libvirt/dnsmasq/default.hostsfile \
---addn-hosts=/var/lib/libvirt/dnsmasq/default.addnhosts \
---enable-ra\

tests/networkxml2argvdata/dhcp6host-routed-network.argv
@@ -1,13 +1,0 @@
-@DNSMASQ@ \
---strict-order \
---domain-needed \
---local=// \
---conf-file= \
---bind-dynamic \
---interface virbr1 \
---dhcp-range 192.168.122.1,static \
---dhcp-no-override \
---dhcp-range 2001:db8:ac10:fd01::1,static \
---dhcp-hostsfile=/var/lib/libvirt/dnsmasq/local.hostsfile \
---addn-hosts=/var/lib/libvirt/dnsmasq/local.addnhosts \
---enable-ra\

tests/networkxml2argvdata/isolated-network.argv
@@ -1,16 +1,0 @@
-@DNSMASQ@ \
---strict-order \
---domain-needed \
---local=// \
---conf-file= \
---bind-interfaces \
---except-interface lo \
---listen-address 192.168.152.1 \
---dhcp-option=3 \
---no-resolv \
---dhcp-range 192.168.152.2,192.168.152.254 \
---dhcp-no-override \
---dhcp-leasefile=/var/lib/libvirt/dnsmasq/private.leases \
---dhcp-lease-max=253 \
---dhcp-hostsfile=/var/lib/libvirt/dnsmasq/private.hostsfile \
---addn-hosts=/var/lib/libvirt/dnsmasq/private.addnhosts\

tests/networkxml2argvdata/nat-network-dns-hosts.argv
@@ -1,10 +1,0 @@
-@DNSMASQ@ \
---strict-order \
---domain-needed \
---domain=example.com \
---expand-hosts \
---local=/example.com/ \
---conf-file= \
---bind-dynamic \
---interface virbr0 \
---addn-hosts=/var/lib/libvirt/dnsmasq/default.addnhosts\

commit cb8974b923e3c40cde96f0c7bceaa638f7f9c72b

network: reject line breaks before writing dnsmasq DNS config
The parser now rejects line breaks in typed DNS TXT and SRV fields,
but the dnsmasq configuration emitter is the actual trust boundary:
any future parser gap, or a value reaching the emitter by another
path, would again let a typed DNS field inject an arbitrary dnsmasq
directive.

Add a defensive check in networkDnsmasqConfContents() that rejects
LF and CR in every typed DNS string immediately before it is written
to the line-based configuration file. This sits behind the parser
checks and keeps the emitter correct on its own.

The raw <dnsmasq:options> namespace is intentionally left untouched:
it is the documented escape hatch for arbitrary dnsmasq options, and
sanitizing it would be a separate, deliberate behavior change.

CVE-2026-61477

Fixes: 8b32c80df089 ("network: put dnsmasq parameters in conf-file instead of command line")
Reviewed-by: Daniel P. Berrangé <berrange@redhat.com>
Signed-off-by: Michael Bommarito <michael.bommarito@gmail.com>

src/network/bridge_driver.c
@@ -64,6 +64,7 @@
 #include "virhook.h"
 #include "virjson.h"
 #include "virnetworkportdef.h"
+#include "virstring.h"
 #include "virutil.h"
 #include "virsystemd.h"
 #include "netdev_bandwidth_conf.h"
@@ -117,6 +118,22 @@ networkDnsmasqDefNamespaceFree(void *nsdata)
     g_free(def);
 }
 G_DEFINE_AUTOPTR_CLEANUP_FUNC(networkDnsmasqXmlNsDef, networkDnsmasqDefNamespaceFree);
+
+
+static int
+networkDnsmasqConfCheckLineBreaks(const char *record,
+                                  const char *field,
+                                  const char *value)
+{
+    if (virStringHasChars(value, "\r\n")) {
+        virReportError(VIR_ERR_CONFIG_UNSUPPORTED,
+                       _("DNS %1$s record %2$s must not contain line breaks"),
+                       record, field);
+        return -1;
+    }
+
+    return 0;
+}
 
 
 static int
@@ -1300,6 +1317,10 @@ networkDnsmasqConfContents(virNetworkObj *obj,
 
     if (wantDNS) {
         for (i = 0; i < dns->ntxts; i++) {
+            if (networkDnsmasqConfCheckLineBreaks("TXT", "name", dns->txts[i].name) < 0 ||
+                networkDnsmasqConfCheckLineBreaks("TXT", "value", dns->txts[i].value) < 0)
+                return -1;
+
             virBufferAsprintf(&configbuf, "txt-record=%s,%s\n",
                               dns->txts[i].name,
                               dns->txts[i].value);
@@ -1321,6 +1342,13 @@ networkDnsmasqConfContents(virNetworkObj *obj,
                                def->name);
                 return -1;
             }
+
+            if (networkDnsmasqConfCheckLineBreaks("SRV", "service", dns->srvs[i].service) < 0 ||
+                networkDnsmasqConfCheckLineBreaks("SRV", "protocol", dns->srvs[i].protocol) < 0 ||
+                networkDnsmasqConfCheckLineBreaks("SRV", "domain", dns->srvs[i].domain) < 0 ||
+                networkDnsmasqConfCheckLineBreaks("SRV", "target", dns->srvs[i].target) < 0)
+                return -1;
+
             /* RFC2782 requires that service and protocol be preceded by
              * an underscore.
              */